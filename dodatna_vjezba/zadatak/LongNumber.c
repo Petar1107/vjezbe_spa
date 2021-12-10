16d28d556b2f2b980e77d706310bbda6e8759235
Petar1107 <92747146+Petar1107@users.noreply.github.com>
2021-12-10 14:43:19 +0100

    Update LongNumber.c

--- dodatna_vjezba/zadatak/LongNumber.c
+++ dodatna_vjezba/zadatak/LongNumber.c
@@ -1,18 +1,19 @@
 #include "longnumber.h"
-//promini
 LongNumber reverse(LongNumber num)
 {
-	LongNumber okrenuti=NULL;
-	while (num != NULL)
+	LongNumber predhodni = NULL;
+	LongNumber trenutni = num;
+	LongNumber sljedeci = NULL;
+	while (trenutni != NULL) 
 	{
-		LongNumber preb = num;
-		num = num->next;
-		preb->next = okrenuti;
-		okrenuti = preb;
+		sljedeci = trenutni->next;
+		trenutni->next = predhodni;
+		predhodni = trenutni;
+		trenutni = sljedeci;
 	}
-	return okrenuti;
+	num = predhodni;
+	return num;
 }
-
 LongNumber dodaj_u_glavu(LongNumber lst, int broj)
 {
 	LongNumber novi = (LongNumber)malloc(sizeof(Digit));
@@ -30,10 +31,13 @@ LongNumber read_longnum(char* fname)
 	int c;
 	for (c = getc(f); c != EOF; c = getc(f))
 	{
-		c = c - 48;
-		num = dodaj_u_glavu(num, c);
+		if (isdigit(c))
+		{
+			c = c - 48;
+			num = dodaj_u_glavu(num, c);
+		}
 	}
-	num = reverse(num);
+	num=reverse(num);
 	return num;
 }
 void write_longnum(LongNumber num, char* fname)
@@ -64,58 +68,24 @@ void delete_longnum(LongNumber num)
 		free(tmp);
 	}
 }
-LongNumber dodaj_na_kraj(LongNumber lst, int broj) {
-	LongNumber novi = (LongNumber)malloc(sizeof(Digit));
-	novi->z = broj;
-	novi->next = NULL;
-
-	if (lst == NULL) {
-		return novi;
-	}
-
-	LongNumber tmp = lst;
-	while (tmp->next != NULL) {
-		tmp = tmp->next;
-	}
-
-	tmp->next = novi;
-	return lst;
-}
-void izednacava(LongNumber a, LongNumber b)
-{
-	//Izjednacavanje
-	while (a!= NULL || b!= NULL) {
-		if (a == NULL) {
-			a=dodaj_u_glavu(a, 0);
-		}
-		if (b == NULL) {
-			b = dodaj_u_glavu(b, 0);
-		}
-		a = a->next;
-		b = b->next;
-	}
-}
 LongNumber add_longnum(LongNumber a, LongNumber b)
 {
 	LongNumber c = NULL;
-	int ostatak=0;
-	a = reverse(a);
-	b = reverse(b);
-	while (a != NULL || b != NULL)
+	int ostatak = 0;
+	LongNumber tmpA = a;
+	LongNumber tmpB = b;
+	tmpA = reverse(tmpA);
+	tmpB = reverse(tmpB);
+	LongNumber tmptmpA = tmpA;
+	LongNumber tmptmpB = tmpB;
+
+	while (tmpA != NULL && tmpB != NULL)
 	{
-		if (a == NULL)
-		{
-			a = dodaj_u_glavu(a, 0);
-		}
-		if (b == NULL)
-		{
-			b = dodaj_u_glavu(b, 0);
-		}
-		int broj = a->z + b->z;
+		int broj = tmpA->z + tmpB->z;
 		if (broj + ostatak > 9)
 		{
 			int znam = broj % 10;
-			c = dodaj_u_glavu(c, (znam + ostatak)%10);
+			c = dodaj_u_glavu(c, (znam + ostatak) % 10);
 			ostatak = 1;
 		}
 		else
@@ -123,54 +93,87 @@ LongNumber add_longnum(LongNumber a, LongNumber b)
 			c = dodaj_u_glavu(c, broj + ostatak);
 			ostatak = 0;
 		}
-		a = a->next;
-		b = b->next;
+		tmpA = tmpA->next;
+		tmpB = tmpB->next;
 	}
+	while (tmpA != NULL) {
+		//a jos postoji
+		c = dodaj_u_glavu(c, (tmpA->z + ostatak) % 10);
+		if (a->z + ostatak > 9) {
+			ostatak = 1;
+		}
+		else {
+			ostatak = 0;
+		}
+		tmpA = tmpA->next;
+
+	}
+	while (tmpB != NULL) {
+		c = dodaj_u_glavu(c, (tmpB->z + ostatak) % 10);
+		if (tmpB->z + ostatak > 9) {
+			ostatak = 1;
+		}
+		else {
+			ostatak = 0;
+		}
+		tmpB = tmpB->next;
+	}
+
 	if (ostatak)
 	{
 		c = dodaj_u_glavu(c, ostatak);
 	}
+	delete_longnum(tmpA);
+	delete_longnum(tmpB);
+	a = reverse(tmptmpA);
+	b = reverse(tmptmpB);
 	return c;
 }
-LongNumber mul_by_digit(LongNumber num, int x)
+LongNumber mul_by_digit(LongNumber num, int z)
 {
 	LongNumber numd = NULL;
+	LongNumber tmp = num;
+	tmp = reverse(tmp);
+	LongNumber tmptmp = tmp;
 	int ostatak = 0;
-	num = reverse(num);
-	while (num != NULL)
+	while (tmp != NULL)
 	{
-		if (num->z * x > 9)
+		if (tmp->z * z > 9)
 		{
-			int znam = (num->z * x) % 10;
+			int znam = (tmp->z * z) % 10;
 			numd = dodaj_u_glavu(numd, (znam + ostatak) % 10);
-			ostatak = (num->z * x +ostatak)/10;
+			ostatak = (tmp->z * z + ostatak) / 10;
 		}
 		else
 		{
-			numd = dodaj_u_glavu(numd, (num->z * x) + ostatak);
+			numd = dodaj_u_glavu(numd, (tmp->z * z) + ostatak);
 			ostatak = 0;
 		}
-		num = num->next;
+		tmp = tmp->next;
 	}
 	if (ostatak)
 	{
 		numd = dodaj_u_glavu(numd, ostatak);
 	}
+	num = reverse(tmptmp);
+	delete_longnum(tmp);
 	return numd;
 }
 LongNumber mul_by_pow10(LongNumber num, int pow)
 {
+	LongNumber tmp = num;
 	LongNumber nume = NULL;
-	while (num != NULL)
+	while (tmp != NULL)
 	{
-		nume = dodaj_u_glavu(nume, num->z);
-		num = num->next;
+		nume = dodaj_u_glavu(nume, tmp->z);
+		tmp = tmp->next;
 	}
 	for (int i = 0; i < pow; i++)
 	{
 		nume = dodaj_u_glavu(nume, 0);
 	}
 	nume = reverse(nume);
+	delete_longnum(tmp);
 	return nume;
 }
 
@@ -178,16 +181,22 @@ LongNumber mul_longnum(LongNumber numa, LongNumber numb)
 {
 	LongNumber pomocni = NULL;
 	LongNumber numf = NULL;
+	LongNumber tmpA = numa;
+	LongNumber tmpB = numb;
 	int br = 0;
 	int i = 0;
-	numb = reverse(numb);
-	while (numb != NULL) 
+	tmpB = reverse(tmpB);
+	LongNumber tmptmpB = tmpB;
+	while (tmpB != NULL)
 	{
-		pomocni = mul_by_pow10(numa, i);
-		pomocni = mul_by_digit(pomocni, numb->z);
+		pomocni = mul_by_pow10(tmpA, i);
+		pomocni = mul_by_digit(pomocni, tmpB->z);
 		i++;
 		numf = add_longnum(numf, pomocni);
-		numb = numb->next;
+		tmpB = tmpB->next;
 	}
+	tmptmpB = reverse(tmpB);
+	delete_longnum(tmpB);
+	delete_longnum(pomocni);
 	return numf;
 }
