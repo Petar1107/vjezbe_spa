ed8eaeb96bdd00f54aff7353629cb4ab95ed1373
Petar1107 <92747146+Petar1107@users.noreply.github.com>
2021-12-10 14:44:12 +0100

    Update main.c

--- dodatna_vjezba/zadatak/main.c
+++ dodatna_vjezba/zadatak/main.c
@@ -9,13 +9,10 @@ int main(void)
 	write_longnum(numb, "upisana_b.txt");
 	LongNumber numc = add_longnum(numa, numb);
 	print_longnum(numc);
-	numa = read_longnum("a.txt");
-	LongNumber numd = mul_by_digit(numa, 9);
+	LongNumber numd = mul_by_digit(numc, 9);
 	print_longnum(numd);
 	LongNumber nume = mul_by_pow10(numd, 3);
 	print_longnum(nume);
-	numa = read_longnum("a.txt");
-	numb = read_longnum("b.txt");
 	LongNumber numf = mul_longnum(numa, numb);
 	print_longnum(numf);
 	delete_longnum(numa);
